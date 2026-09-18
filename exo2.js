e40b00a075015b34caeb8ad4306cb949fee45f41
yox101 <yohanralambo@gmail.com>
2026-09-18 14:18:52 +0300

    suite de l'exo 2

--- exo2.js
+++ exo2.js
@@ -1,18 +1,36 @@
 function echanger(T, ind1, ind2) {
   let temp = T[ind1];
-    T[ind1] = T[ind2];
+  T[ind1] = T[ind2];
   T[ind2] = temp;
-  for (i=0; i<T.lenght; i++){
-    if (T[i]==ind1){
-        T[i]=T[ind2]
-    }else if (T[i]==ind2){
-        T[i]=temp
+  return T;
+}
+
+function ranger(T) {
+  let curseur = 0; 
+  for (let i = 0; i < T.length; i++) {
+    if (T[i] === 0) {
+      echanger(T, i, curseur);
+      curseur++;
     }
   }
-
   return T;
 }
 
 
-T=[1, 0, 0];
-console.log(echanger(T, 0, 1))
+function nombreUn(T) {
+  for (let i = 0; i < T.length; i++) {
+    if (T[i] === 1) {
+      return T.length - i;
+    }
+  }
+  return 0;
+}
+
+let tab1 = [1, 0, 0];
+console.log(echanger(tab1, 0, 1));
+
+let tab2 = [0, 1, 0, 0, 1, 0];
+console.log(ranger(tab2));
+
+console.log(nombreUn(tab2));
+console.log(nombreUn([1, 1, 1]));
